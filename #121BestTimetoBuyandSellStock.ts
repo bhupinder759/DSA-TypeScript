fee76497b0bd4752a85c7d5efff47d5a9e63f7f8
Bhupinder Singh <bhupinder@Bhupinders-MacBook-Air.local>
2026-09-23 19:39:23 +0530

    day 23 sep push

---
+++ #121BestTimetoBuyandSellStock.ts
@@ -0,0 +1,18 @@
+function maxProfit(prices: number[]): number {
+    let minPrice = prices[0];
+    let maxProfit = 0;
+
+    for (let i = 1; i < prices.length; i++) {
+        if (prices[i] < minPrice) {
+            minPrice = prices[i];
+        }
+
+        const profit = prices[i] - minPrice;
+
+        if (profit > maxProfit) {
+            maxProfit = profit;
+        }
+    }
+
+    return maxProfit;
+};
